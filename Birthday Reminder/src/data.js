000ed739d769dd29dca9b1dcfee3aca190a0c198
kashishbagadia <kashishbagadiamd06@gmail.com>
2021-05-18 21:57:41 +0530

    Files

---
+++ Birthday Reminder/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Harry Bing',
+    age: 19,
+    image:
+      'https://images.unsplash.com/photo-1595288573661-d0e6a92d8616?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+  },
+  {
+    id: 2,
+    name: 'Alice Morgan',
+    age: 32,
+    image:
+      'https://images.unsplash.com/photo-1592573394845-e7ed2cd7042e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+  },
+  {
+    id: 3,
+    name: 'Rachel Geller',
+    age: 26,
+    image:
+      'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
+  },
+  {
+    id: 4,
+    name: 'Rubal Bansal',
+    age: 3,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
